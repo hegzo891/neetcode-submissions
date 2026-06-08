f590ed4c5e2eda405dfc484e0d3f3fdf7ad6d5ca
Ahmed hegazy <121024792+hegzo891@users.noreply.github.com>
2026-06-09 01:11:13 +0300

    Add: is-subsequence - submission-11

---
+++ Data Structures & Algorithms/is-subsequence/submission-11.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        for i in range(len(s)):
+            if s[i] in t:
+                pos = t.find(s[i])
+                t = t[pos + 1 :]
+            else:
+                return False
+        return True
